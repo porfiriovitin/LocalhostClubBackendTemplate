2719f1d2f6459486f3648bcb85d3fb573ed873db
Victor Hugo Porfirio <victorhugo.svm@hotmail.com.br>
2026-02-05 17:52:10 -0300

    feat: secure logout and user info routes with authentication middleware

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user-controller.js";
+import { ensureAuthenticated } from "../middlewares/ensure-authenticated.js";
 
 const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.post("/register", userController.register);
 userRoutes.post("/login", userController.login);
-userRoutes.post("/logout", userController.logout);
+userRoutes.post("/logout", ensureAuthenticated, userController.logout);
 
-userRoutes.get("/me", userController.userInfo);
+userRoutes.get("/me", ensureAuthenticated, userController.userInfo);
 
 export { userRoutes };
